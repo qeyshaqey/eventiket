e83c286e7cbb82d34309c33e9a3f7a2a54769f3a
naya <nayakhairunnisa24012011@gmail.com>
2026-04-27 16:54:11 +0700

    menyelaraskan warna pada tampilan dashboard admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <!-- Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <style>
         body {
-            font-family: 'Plus Jakarta Sans', sans-serif;
+            font-family: 'Poppins', sans-serif;
         }
     </style>
 </head>
